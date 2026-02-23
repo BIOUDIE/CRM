0e59026a5e3c9631f9366700a3714340534134c5
BIOUDIE <41976717+BIOUDIE@users.noreply.github.com>
2026-02-23 17:05:41 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      borderRadius: {
+        '3xl': '1.5rem',
+      },
+    },
   },
   plugins: [],
 }
